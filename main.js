3e24801817fdf4fc8ee7f26c118a83f8fafeb59e
Nadia <nadiyaklokova@gmail.com>
2021-08-03 15:45:10 +0200

    limpieza js, html > inputs fuera

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const btn = document.querySelector('input');
 const body = document.querySelector('body');
 
-
-function chBackcolor(color) {
+/* function chBackcolor(color) {
     document.body.style.background = color;
-}
-
-
+} */
 
 let icecream = document.querySelector('.icecream');
 let icecream_body = document.querySelector('.icecream_body');
@@ -16,26 +13,12 @@ let drip_midle_small = document.querySelector('.icecream__drip--midle--small');
 let drip_small = document.querySelector('.icecream__drip--small');
 
 
-/* btn.addEventListener('change', cambiarColor); */
-/* function cambiarColor(){
-    let election = btn.value;
-
-    if(election === 'rgb(253,253,137)'){
-        icecream.style.background = 'yellow';
-    } else if (election === 'rgb(251, 164, 198)'){
-        icecream.style.background = 'red';
-    } else if (election === 'rgb(144, 210, 253)'){
-        icecream.style.background = 'blue';
-    }else{
-        icecream.style.background = 'green';
-    }
-} */
-
 function cambiarColor(color){
 
     console.log(color);
 
-
+    document.body.style.background = color;
+    
     switch(color){
         case 'rgb(253, 253, 137, 0.581)':
         icecream.style.background = 'yellow';
@@ -73,3 +56,20 @@ function cambiarColor(color){
         icecream_body.style.background = 'rgba(164, 243, 127)';  
     }
 }
+
+
+
+/* btn.addEventListener('change', cambiarColor); */
+/* function cambiarColor(){
+    let election = btn.value;
+
+    if(election === 'rgb(253,253,137)'){
+        icecream.style.background = 'yellow';
+    } else if (election === 'rgb(251, 164, 198)'){
+        icecream.style.background = 'red';
+    } else if (election === 'rgb(144, 210, 253)'){
+        icecream.style.background = 'blue';
+    }else{
+        icecream.style.background = 'green';
+    }
+} */
